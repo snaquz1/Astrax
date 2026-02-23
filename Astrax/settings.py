5379269e057bb5832eeb837d8ffefffc37d45d9c
777 <a@>
2026-02-23 19:48:50 +0300

    fix

--- Astrax/settings.py
+++ Astrax/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "MainApp.apps.MainappConfig",
-    "Users.apps.UsersConfig",
+    "Users",
 ]
 
 MIDDLEWARE = [
