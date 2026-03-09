cef0f900618378f2775c0e48ec89439c2bd9782d
777 <a@>
2026-03-09 19:31:04 +0300

    deploy fix 2

--- Chat/routing.py
+++ Chat/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path, path
 from .consumers import *
 
 websocket_urlpatterns = [
-    path("ws/chat/<int:chat_id>/", ChatConsumer.as_asgi())
+    path("ws/chat/<int:chat_id>/", ChatConsumer.as_asgi()),
     path("wss/chat/<int:chat_id>/", ChatConsumer.as_asgi())
 ]
