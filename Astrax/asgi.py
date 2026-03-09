60bb970e19dbf3f700741626af4b77a26a995de3
777 <a@>
2026-03-09 19:47:24 +0300

    deploy fix 3

--- Astrax/asgi.py
+++ Astrax/asgi.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 
 import os
 print("🔥 ASGI LOADED 🔥")
-from channels.auth import AuthMiddleware, AuthMiddlewareStack
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Astrax.settings')
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
 import Chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Astrax.settings')
+
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
